74161296e8991135e3222fbb6303979360483d4f
nico juniorcloudllc <51682061+cloudcover95@users.noreply.github.com>
2026-09-14 00:25:06 -0600

    feat: Linux app + Swift rail goldens

---
+++ rails/ios/Handshake.swift
@@ -0,0 +1,8 @@
+// Spec rail. Not an App Store binary.
+// Envelope: goldend-osai-omega/1
+public struct GaiaHandshake {
+    public var protocolId = "goldend-osai-omega/1"
+    public var orient = "portrait"
+    public var ue5Launch = false
+    public var download = false
+}
